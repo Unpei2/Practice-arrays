d3daa453c8219921244d52c85a00556be4614e16
Unpei2 <r.ho5@share.epsb.ca>
2022-10-26 14:50:29 -0600

    done

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-console.log("hi")
+let months = ["January", "June", "July"]
+
+let joyarray = []
+for (x = 0; x < 700;x++){
+    joyarray.push("joy")
+}
+
+let sevenarray = []
+for (x = 0; x < 500 ;x++){
+    sevenarray.push(7)
+}
+
+// let randarray = []
+// for (x = 0; x < 5000; x++){
+//     randarray.push(Math.random() * (100))
+// }
+
+let secondrandarray = []
+for (x = 0; x < 300; x++){
+    secondrandarray.push(Math.random() * (40))
+}
+
+let multiplesarray = []
+for (x = 20 ;x < 804; x += 4){
+    multiplesarray.push(x)
+}
+
+let evennumbers = []
+for (x = 100;x > 8;x -= 2){
+    evennumbers.push(x)
+}
+
+let colorsStr = "red,orange,yellow,green,blue,indigo,violet"
+colorsStr.split(",")
+
+let citiesStr = "Edmonton;Calgary;Vancouver;Saskatoon;Winnipeg"
+colorsStr.split(";")
+
+
+let namse = []
+let loop = true
+while (loop){
+    let answer = prompt("enter a name")
+    if (answer === "done"){
+        loop = false
+    }
+}
+// function check(){
+//     let addname = prompt("please add a name")
+//     namse.push(addname)
+//     console.log(namse)
+//     if (addname !== "done"){
+//         check()
+//     }
+// }
+// check()
